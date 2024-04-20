9ddb08df47d5c9c86521f14c2bfea0c9ede32b86
AsgerToft <103510655+AsgerToft@users.noreply.github.com>
2024-04-20 19:41:18 +0200

    Moving some files around
    Put them in the right folders :)

--- Your Soul Purpose/Assets/Scripts/Playermovement.cs
+++ Your Soul Purpose/Assets/Scripts/Playermovement.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-
+    Animator anim;
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
 
     Vector2 movement;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,5 +28,14 @@ public class Playermovement : MonoBehaviour
     {
         //Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void TurnLeft()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+    public void TurnRight()
+    {
+        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
     }
 }
