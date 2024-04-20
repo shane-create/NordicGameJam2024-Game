168c2779fc123ea8c01622b4073d31d51e6d00e2
berrybeb <florafalkenberg@gmail.com>
2024-04-20 22:05:16 +0200

    animations!

--- Your Soul Purpose/Assets/Scripts/Playermovement.cs
+++ Your Soul Purpose/Assets/Scripts/Playermovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Playermovement : MonoBehaviour
 {
 
+    public Animator anim;
+
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
@@ -17,6 +19,23 @@ public class Playermovement : MonoBehaviour
         //Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        bool walking = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.01f;
+        anim.SetBool("walk", walking);
+
+        if (movement.x < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+        else if (movement.x > 0.01f)
+        {
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+
     }
 
     private void FixedUpdate()
